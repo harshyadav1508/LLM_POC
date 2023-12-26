36689120c793a239b8ae8625fbea2236c0c7ab8d
harshyadav1508 <yaduvanshiharsh15@gmail.com>
2023-12-27 03:23:23 +0530

    Added chat bot

--- chat.py
+++ chat.py
@@ -10,19 +10,29 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # function to generate response from gemini pro model
 model = genai.GenerativeModel("gemini-pro")
-
+chat = model.start_chat(history=[])
 
 def get_gemini_response(question):
-    response = model.generate_content(question)
+    response = chat.send_message(question,stream=True)
     return response.text
 
 
 st.set_page_config(page_title="Q&A Demo")
 st.header("Gemini LLM Application")
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] =[]
 input = st.text_input("Input: ", key="input")
 submit = st.button("Ask the question")
 
-if submit:
+if submit and input:
     response = get_gemini_response(input)
-    st.subheader("The resposne is")
-    st.write(response)
+    st.session_state['chat_history'].append(("You", input))
+    st.subheader("The Response is:")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot", chunk.text))
+
+st.subheader("The chat history is")
+
+for role,text in st.session_state['chat_history']:
+    st.write(f"{role}:{text}")
